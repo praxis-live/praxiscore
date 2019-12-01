b0912f9bf1d13467ece33952ce3daf1dee317134
Neil C Smith <neilcsmith.net@googlemail.com>
2019-12-01 12:18:16 +0000

    Add Video GStreamer module.

---
+++ praxiscore-video-gstreamer/src/main/java/module-info.java
@@ -0,0 +1,28 @@
+
+module org.praxislive.video.gstreamer {
+    
+    requires java.logging;
+    
+    requires org.freedesktop.gstreamer;
+    requires com.sun.jna;
+    requires com.sun.jna.platform;
+    
+    requires org.praxislive.core;
+    requires org.praxislive.code;
+    requires org.praxislive.logging;
+    requires org.praxislive.video.code;
+    requires org.praxislive.video;
+    requires org.praxislive.settings;
+    
+    exports org.praxislive.video.gstreamer;
+    exports org.praxislive.video.gstreamer.configuration;
+
+    opens org.praxislive.video.gstreamer.components;
+    opens org.praxislive.video.gstreamer.components.resources;
+    
+    provides org.praxislive.core.services.ComponentFactoryProvider with 
+            org.praxislive.video.gstreamer.components.GStreamerComponents;
+    provides org.praxislive.code.CodeConnector.Plugin with
+            org.praxislive.video.gstreamer.components.GStreamerCodePlugin;
+    
+}
