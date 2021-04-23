9f10ada07d02d53e606162fc72b4ca987138d315
Neil C Smith <neilcsmith.net@googlemail.com>
2021-04-23 17:36:19 +0100

    Update versions for v5.2.0 release (plus Javadoc build fix).

--- praxiscore-audio/src/main/java/module-info.java
+++ praxiscore-audio/src/main/java/module-info.java
@@ -3,8 +3,6 @@ module org.praxislive.audio {
     
     requires org.praxislive.core;
     requires org.jaudiolibs.pipes;
-    requires org.jaudiolibs.audioops;
-    requires org.jaudiolibs.audioservers;
     
     exports org.praxislive.audio;
     
