eb0bc3393343d5e980d479a93258b07457970944
Neil C Smith <neilcsmith.net@googlemail.com>
2022-10-21 15:50:21 +0100

    Update PArray and PMap to use immutable collections and improve docs.

--- praxiscore-api/src/main/java/org/praxislive/core/types/PArray.java
+++ praxiscore-api/src/main/java/org/praxislive/core/types/PArray.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 2020 Neil C Smith.
+ * Copyright 2022 Neil C Smith.
  * 
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 3 only, as
@@ -22,7 +22,6 @@
 package org.praxislive.core.types;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -36,37 +35,59 @@ import org.praxislive.core.syntax.Token;
 import org.praxislive.core.syntax.Tokenizer;
 
 /**
- *
+ * An ordered list of Values.
  */
 public final class PArray extends Value implements Iterable<Value> {
 
-    public final static PArray EMPTY = new PArray(new Value[0], "");
-    private final Value[] data;
+    /**
+     * An empty PArray.
+     */
+    public final static PArray EMPTY = new PArray(List.of(), "");
+
+    private final List<Value> data;
     private volatile String str;
 
-    private PArray(Value[] data, String str) {
-        this.data = data;
+    private PArray(List<Value> data) {
+        this(data, null);
+    }
+
+    private PArray(List<Value> data, String str) {
+        this.data = List.copyOf(data);
         this.str = str;
     }
 
+    /**
+     * Query the value at the given index in the list. If the index is negative
+     * or greater than size, the index is modulated into range rather than
+     * throwing an exception - useful for cycling. If this PArray is empty, this
+     * is returned.
+     *
+     * @param index position of value
+     * @return value at index
+     */
     public Value get(int index) {
-        int count = data.length;
+        int count = data.size();
         if (count > 0) {
             index %= count;
-            return index < 0 ? data[index + count] : data[index];
+            return index < 0 ? data.get(index + count) : data.get(index);
         } else {
             return this;
         }
     }
 
+    /**
+     * Query the number of values in the list.
+     *
+     * @return size of list
+     */
     public int size() {
-        return data.length;
+        return data.size();
     }
-    
+
     @Override
     public String toString() {
         if (str == null) {
-            if (data.length > 0) {
+            if (!data.isEmpty()) {
                 StringBuilder sb = new StringBuilder();
                 for (Value entry : data) {
                     if (sb.length() > 0) {
@@ -92,7 +113,7 @@ public final class PArray extends Value implements Iterable<Value> {
 
     @Override
     public boolean isEmpty() {
-        return data.length == 0;
+        return data.isEmpty();
     }
 
     @Override
@@ -102,12 +123,12 @@ public final class PArray extends Value implements Iterable<Value> {
                 return true;
             }
             PArray other = PArray.coerce(arg);
-            int size = data.length;
-            if (size != other.data.length) {
+            int size = data.size();
+            if (size != other.data.size()) {
                 return false;
             }
             for (int i = 0; i < size; i++) {
-                if (!Utils.equivalent(data[i], other.data[i])) {
+                if (!Utils.equivalent(data.get(i), other.data.get(i))) {
                     return false;
                 }
             }
@@ -119,78 +140,75 @@ public final class PArray extends Value implements Iterable<Value> {
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(data);
+        return data.hashCode();
     }
 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof PArray) {
             PArray o = (PArray) obj;
-            return Arrays.equals(data, o.data);
+            return data.equals(o.data);
         }
         return false;
     }
 
     @Override
     public Iterator<Value> iterator() {
-        return new Itr();
+        return data.iterator();
     }
 
+    /**
+     * An ordered stream over the list of values.
+     *
+     * @return stream of values
+     */
     public Stream<Value> stream() {
-        return Stream.of(data);
+        return data.stream();
     }
 
-    private class Itr implements Iterator<Value> {
-
-        int cursor = 0;
-
-        @Override
-        public boolean hasNext() {
-            return cursor < data.length;
-        }
-
-        @Override
-        public Value next() {
-            Value arg = data[cursor];
-            cursor++;
-            return arg;
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException("PArrays are immutable");
-        }
+    /**
+     * An unmodifiable {@link List} view of this list of values.
+     *
+     * @return view as unmodifiable list
+     */
+    public List<Value> asList() {
+        return data;
     }
 
+    /**
+     * Create a PArray from the given collection of values.
+     *
+     * @param collection collection of values
+     * @return new PArray
+     */
     public static PArray of(Collection<? extends Value> collection) {
-        return valueOf(collection.toArray(new Value[collection.size()]), false);
+        return new PArray(List.copyOf(collection));
     }
 
-    public static PArray of(Value... args) {
-        return valueOf(args, true);
-    }
-    
-    private static PArray valueOf(Value[] args, boolean clone) {
-        if (args.length == 0) {
-            return PArray.EMPTY;
-        }
-        Value[] safeArgs = clone ? args.clone() : args;
-        for (Value v : safeArgs) {
-            if (v == null) {
-                throw new NullPointerException();
-            }
-        }
-        return new PArray(args, null);
+    /**
+     * Create a PArray from the given collection of values.
+     *
+     * @param values array of values
+     * @return new PArray
+     */
+    public static PArray of(Value... values) {
+        return new PArray(List.of(values));
     }
 
-    public static PArray parse(String str) throws ValueFormatException {
-        if (str.length() == 0) {
+    /**
+     * Parse the given text into a PArray.
+     *
+     * @param text text to parse
+     * @return parsed PArray
+     * @throws ValueFormatException
+     */
+    public static PArray parse(String text) throws ValueFormatException {
+        if (text.length() == 0) {
             return PArray.EMPTY;
         }
         try {
-            Tokenizer tk = new Tokenizer(str);
-            List<PString> list = new ArrayList<PString>();
-            tokenize:
+            List<Token> tk = Tokenizer.parse(text);
+            List<Value> list = new ArrayList<>();
             for (Token t : tk) {
                 Token.Type type = t.getType();
                 switch (type) {
@@ -213,7 +231,7 @@ public final class PArray extends Value implements Iterable<Value> {
             if (size == 0) {
                 return PArray.EMPTY;
             } else {
-                return new PArray(list.toArray(new Value[size]), str);
+                return new PArray(list, text);
             }
         } catch (Exception ex) {
             throw new ValueFormatException(ex);
@@ -229,18 +247,40 @@ public final class PArray extends Value implements Iterable<Value> {
         }
     }
 
-    public static Optional<PArray> from(Value arg) {
+    /**
+     * Cast or convert the provided value into a PArray, wrapped in an Optional.
+     * If the value is already a PArray, the Optional will wrap the existing
+     * value. If the value is not a PArray and cannot be converted into one, an
+     * empty Optional is returned.
+     *
+     * @param value value
+     * @return optional PArray
+     */
+    public static Optional<PArray> from(Value value) {
         try {
-            return Optional.of(coerce(arg));
+            return Optional.of(coerce(value));
         } catch (ValueFormatException ex) {
             return Optional.empty();
         }
     }
 
+    /**
+     * Utility method to create an {@link ArgumentInfo} for arguments of type
+     * PArray.
+     *
+     * @return argument info
+     */
     public static ArgumentInfo info() {
         return ArgumentInfo.of(PArray.class, null);
     }
 
+    /**
+     * Create a {@link Collector} that can create a PArray from a Stream of
+     * Values.
+     *
+     * @param <T> Value type
+     * @return new PArray collector
+     */
     public static <T extends Value> Collector<T, ?, PArray> collector() {
 
         return Collector.<T, List<T>, PArray>of(ArrayList::new,
--- praxiscore-api/src/test/java/org/praxislive/core/types/PMapTest.java
+++ praxiscore-api/src/test/java/org/praxislive/core/types/PMapTest.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.praxislive.core.types;
 
-import org.praxislive.core.types.PMap;
-import org.praxislive.core.Value;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -53,7 +46,7 @@ public class PMapTest {
 
     @Test
     public void testParse() throws Exception {
-        PMap m1 = PMap.builder(4)
+        PMap m1 = PMap.builder()
                 .put("key1", "value1")
                 .put("key2", 2)
                 .put("key3", true)
