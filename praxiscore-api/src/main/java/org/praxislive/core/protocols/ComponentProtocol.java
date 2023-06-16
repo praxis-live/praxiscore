90009a6134628d4892b4c1853c574b4a2a7bbeef
Neil C Smith <neilcsmith.net@googlemail.com>
2023-06-16 19:24:27 +0100

    Add documentation to core protocols, deprecate instances and cleanup.

--- praxiscore-api/src/main/java/org/praxislive/core/protocols/ComponentProtocol.java
+++ praxiscore-api/src/main/java/org/praxislive/core/protocols/ComponentProtocol.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2020 Neil C Smith.
+ * Copyright 2023 Neil C Smith.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 3 only, as
@@ -22,32 +22,49 @@
 package org.praxislive.core.protocols;
 
 import java.util.stream.Stream;
+import org.praxislive.core.Component;
 import org.praxislive.core.ComponentInfo;
 import org.praxislive.core.ControlInfo;
 import org.praxislive.core.Info;
 import org.praxislive.core.Protocol;
 
 /**
- *
+ * Basic component protocol, providing access to the component info.
  */
 public class ComponentProtocol implements Protocol {
-    
+
+    @Deprecated
     public final static ComponentProtocol INSTANCE = new ComponentProtocol();
+
+    /**
+     * Name of the info control.
+     */
     public final static String INFO = "info";
-    public final static ControlInfo INFO_INFO =
-            ControlInfo.createReadOnlyPropertyInfo(
-            ComponentInfo.info(),
-            null);
+
+    /**
+     * Control info for the info control. A read-only property that returns the
+     * component info. The response to calling this control should be the same
+     * as calling {@link Component#getInfo}.
+     */
+    public final static ControlInfo INFO_INFO
+            = ControlInfo.createReadOnlyPropertyInfo(
+                    ComponentInfo.info(),
+                    null);
+
+    /**
+     * A component info for this protocol. Can be used with
+     * {@link Info.ComponentInfoBuilder#merge(org.praxislive.core.ComponentInfo)}.
+     */
     public final static ComponentInfo API_INFO = Info.component(cmp -> cmp
             .protocol(ComponentProtocol.class)
             .control(INFO, INFO_INFO)
     );
-    
+
     @Override
     public Stream<String> controls() {
         return Stream.of(INFO);
     }
-    
+
     @Override
     public ControlInfo getControlInfo(String control) {
         if (INFO.equals(control)) {
@@ -55,5 +72,5 @@ public class ComponentProtocol implements Protocol {
         }
         throw new IllegalArgumentException();
     }
-    
+
 }
