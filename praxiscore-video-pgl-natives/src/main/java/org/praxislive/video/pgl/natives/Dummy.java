4f13619d2fd9506e8cf4d4907058d85c0694b174
Neil C Smith <neilcsmith.net@googlemail.com>
2024-08-26 14:52:30 +0100

    Add CORE binaries and natives JAR to deploy.

---
+++ praxiscore-video-pgl-natives/src/main/java/org/praxislive/video/pgl/natives/Dummy.java
@@ -0,0 +1,12 @@
+package org.praxislive.video.pgl.natives;
+
+/**
+ * This class is only here to allow this module to pass staging requirements.
+ */
+public final class Dummy {
+
+    private Dummy() {
+
+    }
+
+}
