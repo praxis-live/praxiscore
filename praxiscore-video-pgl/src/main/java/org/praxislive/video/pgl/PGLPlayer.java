6ba2ca417c335e24198fe3415f5ccd0321e1b3aa
Neil C Smith <neilcsmith.net@googlemail.com>
2026-01-26 18:24:07 +0000

    Remove always on top flag from video window before closing.
    Issue with macOS dialogs and other parts of the IDE window being unusable
    after an overlapping video window is closed appears to be related to the
    always on top behaviour.

--- praxiscore-video-pgl/src/main/java/org/praxislive/video/pgl/PGLPlayer.java
+++ praxiscore-video-pgl/src/main/java/org/praxislive/video/pgl/PGLPlayer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 2025 Neil C Smith.
+ * Copyright 2026 Neil C Smith.
  * 
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 3 only, as
@@ -438,6 +438,7 @@ public class PGLPlayer implements Player {
         public synchronized void dispose() {
             sink.disconnect();
             context.dispose();
+            surface.setAlwaysOnTop(false);
             super.dispose();
         }
 
